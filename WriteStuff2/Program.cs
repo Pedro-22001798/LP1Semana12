e69e845579ae7f15d8b8241a05218782b7100fb3
Pedro Silva <pedroafonsosilva.art@gmail.com>
2023-06-02 11:57:31 +0100

    Change Program to StreamWriter

--- WriteStuff2/Program.cs
+++ WriteStuff2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace WriteStuff2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please write a file's name to search.");
+                return;
+            }            
+            string filename = args[0];
+
+            StreamWriter sw = new StreamWriter(filename);     
+
+            while(true)
+            {
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    sw.Close();
+                    break;
+                }
+
+                sw.WriteLine(input);
+            }  
+   
+        }
+    }
+}
