e204eac97c4ded47c907032359a13a1102260910
Pedro Silva <pedroafonsosilva.art@gmail.com>
2023-06-02 11:53:44 +0100

    Create Queue and File.WriteAllLines

--- WriteStuff1/Program.cs
+++ WriteStuff1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace WriteStuff1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please write a file's name to search.");
+                return;
+            }            
+            string filename = args[0];
+
+            Queue<string> stringsQueue = new Queue<string>();       
+
+            while(true)
+            {
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+
+                stringsQueue.Enqueue(input);
+            }  
+
+            File.WriteAllLines(filename,stringsQueue);   
+        }
+    }
+}
